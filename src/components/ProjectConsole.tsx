527dd32eb0bf2d12a182353939150626495fd3af
kc-1 <84696121+kelvinkaifungchan@users.noreply.github.com>
2023-12-01 10:06:57 +0000

    fixed panel spacing

--- src/components/ProjectConsole.tsx
+++ src/components/ProjectConsole.tsx
@@ -11,30 +11,33 @@ import { AnimatePresence } from "framer-motion";
 interface ProjectConsoleProps {
   projects: Project[];
   name: string | undefined;
-  portfolioName: string | undefined
+  portfolioName: string | undefined;
 }
 
 export const ProjectConsole: React.FC<ProjectConsoleProps> = ({
   projects,
   name,
-  portfolioName
+  portfolioName,
 }) => {
   const [currentProject, setCurrentProject] = useState(0);
 
   return (
     <>
       <main className="flex h-full w-full font-light overflow-y-hidden overflow-x-hidden">
-        <div className="w-full h-full flex flex-col">
-          <div className="text-xl p-5 border-r border-[#FFFFFF55]">{name} ({portfolioName})</div>
+        <div className="w-full h-full flex flex-col min-w-[30vw] w-[30vw]">
+          <div className="text-xl p-5 border-r border-[#FFFFFF55]">
+            {name} ({portfolioName})
+          </div>
           <HorizontalLine />
-          <div className="pr-5 p-5 space-y-3 border-r border-[#FFFFFF55] h-full min-w-[30vw] w-[30vw] overflow-y-auto customScroll">
+          <div className="pr-5 p-5 space-y-3 border-r border-[#FFFFFF55] h-full  overflow-y-auto customScroll">
             <div>
               <div className="italic text-xl mb-2">Projects</div>
               <div className="italic text-xs">
                 Unless otherwise specified, all images presented in this
-                portfolio have been created and are owned by {name}. Any unauthorized use, reproduction, or distribution of
-                these images without the explicit permission of the author is
-                strictly prohibited.
+                portfolio have been created and are owned by {name}. Any
+                unauthorized use, reproduction, or distribution of these images
+                without the explicit permission of the author is strictly
+                prohibited.
               </div>
             </div>
             {projects?.map((project, index) => {
